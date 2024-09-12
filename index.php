0c3940701179e391476fa3be8e3160bc746c9764
mansesrivas <manasesnehemiasr@gmail.com>
2024-09-12 13:47:38 -0300

    codigo optimizado

--- index.php
+++ index.php
@@ -1,39 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Renta de vehiculos</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-</head>
-<body>
-    <section class="container">
-        <div class="encabezado">
-            <h2>Software para el alquiler de vehiculos</h2>
-        </div>
-        <form action="procesar.php" class="container__form" autocomplete="off" method="post">
-            <h3 class="subtitle text_strong">Datos cliente</h3>
+<?php 
+    $content = '            <h3 class="subtitle text_strong">Datos cliente</h3>
             <div class="form__inputs">
                 <label for="" class="form__label text_strong">Nombre del cliente:</label>
-                <input type="text" name="nombre_cliente" class="form__input" required>
+                <input type="text" name="Nombre_del_cliente" class="form__input" required>
             </div>
             <div class="form__inputs">
                 <label for="" class="form__label text_strong">Direccion:</label>
-                <input type="text" name="direccion" id="" class="form__input" required>
+                <input type="text" name="Direccion" id="" class="form__input" required>
             </div>
             <div class="form__inputs">
                 <label for="" class="form__label text_strong">Telefono:</label>
-                <input type="number" name="telefono" id="" class="form__input" required>
+                <input type="number" name="Telefono" id="" class="form__input" required>
             </div>
 
             <h3 class="subtitle text_strong">Datos del alquiler</h3>
 
             <div class="form__inputs">
                 <label for="" class="form__label text_strong">Seleccion del vehiculo</label>
-                <select name="vehiculo" id="" class="selects" required>
+                <select name="vehiculo_rentado" id="" class="selects" required>
 
                     <option value="" selected disabled>Seleccione un vehiculo</option>
                     <option value="Toyota Corolla">Toyota Corolla</option>
@@ -47,12 +31,12 @@
 
             <div class="form__inputs">
                 <label for="" class="form__label text_strong">Fecha inicial</label>
-                <input type="date" name="fecha" id="" required class="form__input">
+                <input type="date" name="Fecha_de_solicitud" id="" required class="form__input">
             </div>
 
             <div class="form__inputs">
                 <label for="" class="form__label text_strong">Dias que se alquilara</label>
-                <select name="cantidad_dias" id="" class="selects">
+                <select name="Dias_que_se_alquilara" id="" class="selects">
 
                     <option value="1">1</option>
                     <option value="2">2</option>
@@ -83,8 +67,6 @@
             <button type="submit" class="button">Calcular pago</button>
             <button type="reset" class="button">Reestablecer</button>
         </div>
-
-        </form>
-    </section>
-</body>
-</html>
+';
+include "plantilla.html";
+?>
--- procesar.php
+++ procesar.php
@@ -1,87 +1,37 @@
-
 <?php
-// Función para calcular el costo total del alquiler
 function calcularCostoAlquiler($dias)
 {
-    if ($dias >= 1 && $dias <= 2) {
-    return $dias * 100;
-    } elseif ($dias >= 3 && $dias <= 5) {
-    return $dias * 90;
-    } elseif ($dias >= 6 && $dias <= 10) {
-    return $dias * 75;
-    } else {
-    return 0; // Si los días están fuera del rango permitido
-    }
+    if ($dias >= 1 && $dias <= 2) return $dias * 100;
+    elseif ($dias >= 3 && $dias <= 5) return $dias * 90;
+    elseif ($dias >= 6 && $dias <= 10) return $dias * 75;
+    else return 0; 
 }
 
-// Capturar los datos del formulario
+$lista = array();
+$sumas = number_format(calcularCostoAlquiler($_POST['Dias_que_se_alquilara']), 2);
 
-
-$lista = [
-    'Nombre del cliente' => htmlspecialchars($_POST["nombre_cliente"]),
-    'Direccion' => htmlspecialchars($_POST["direccion"]),
-    'Telefono' => htmlspecialchars($_POST["telefono"]),
-    'Vehiculo rentado' => htmlspecialchars($_POST["vehiculo"]),
-    'Dias que se alquilara' => (int)$_POST["cantidad_dias"],
-    'Fecha de solicitud' => htmlspecialchars($_POST["fecha"]),
-    'SUMAS' => '',
-    'DESCUENTO' => '',
-    'SUB TOTAL' => '', 
-    '13% IVA' => '',
-    'TOTAL A PAGAR' => '' 
-
-];
-// Calcular el costo del alquiler
-$lista['SUMAS'] = number_format(calcularCostoAlquiler($lista['Dias que se alquilara']), 2);
-
-if ($_POST["metodo_pago"] == "Efectivo") {
-    $lista['DESCUENTO'] = number_format($lista['SUMAS'] * 0.10, 2);
-} else {
-    $lista['DESCUENTO'] = 0;
+foreach ($_POST as $key => $value) {
+    if($key == "metodo_pago") continue; 
+    $lista[str_replace("_", " ", $key)] = $value;
 }
 
+$lista['SUMAS'] = $sumas;
+($_POST["metodo_pago"] == "Efectivo") ? $lista['DESCUENTO'] = number_format($sumas * 0.10, 2) : $lista['DESCUENTO'] = 0;
 $lista['SUB TOTAL'] = number_format($lista['SUMAS'] - $lista['DESCUENTO'], 2) ;
 $lista['13% IVA'] = number_format($lista['SUB TOTAL'] * 0.13, 2);
 $lista['TOTAL A PAGAR'] = number_format($lista['SUB TOTAL'] + $lista['13% IVA'], 2);
 
 $content = '';
 
-// concatenacion de la clave y el valor
 foreach ($lista as $clave => $valor) {
     $content .= '<div class="form__results">
     <p for="" class="form__label">'.$clave.': <span>'.$valor.'</span></p>
     </div>';
 }
 
-
-// imprimir el cuerpo completo del HTML y concatenando la variable "content" que contiene los datos del alquiler
-echo '<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reporte de pago de vehiculo</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-</head>
-<body>
-    <section class="container">
-        <div class="encabezado">
-            <h2>Reporte de pago de alquiler</h2>
-        </div>
-        <div class="container__form">
-
-            '.$content.'
-
-            <div class="form_inputs">
+$content .= '<div class="form_inputs">
                 <a href="index.php"><button type="button" class="button">Registrar un nuevo alquiler</button></a>
+            </div>';
 
-            </div>
-
-        </div>
-    </section>
-</body>
-</html>';
+include "plantilla.html"
 ?>
